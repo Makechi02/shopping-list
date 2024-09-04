c36ce4cdef5f2775fb1409c94df0811d0c6d2362
makechi <makechieric9@gmail.com>
2024-09-04 20:01:22 +0300

    Made it a PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,52 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-});
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Shopping List',
+                short_name: 'ShopList',
+                description: 'A simple shopping list application',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+            workbox: {
+                runtimeCaching: [
+                    {
+                        urlPattern: ({ request }) => request.destination === 'image',
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'images-cache',
+                            expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 7},
+                        },
+                    },
+                    {
+                        urlPattern: ({ request }) =>
+                            request.destination === 'script' ||
+                            request.destination === 'style',
+                        handler: 'StaleWhileRevalidate',
+                        options: {
+                            cacheName: 'assets-cache',
+                            expiration: {maxEntries: 20, maxAgeSeconds: 60 * 60 * 24 * 7},
+                        },
+                    },
+                ],
+            },
+        })
+    ]
+})
